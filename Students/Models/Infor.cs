3236c61112d38481dd887a89dd2f84ee0b88f96b
phamkhacan1903 <phamkhacan1903@gmail.com>
2019-01-16 17:45:36 +0700

    crud-simple

---
+++ Students/Models/Infor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Students.Models
+{
+    public class Infor
+    {
+        [Key]
+        [DisplayName("Mã Sinh Viên")]
+        public int StudentId { get; set; }
+        [DisplayName("Tên Sinh Viên")]
+        public string StudentName { get; set; }
+        [DisplayName("Tuổi")]
+        public int Age { get; set; }
+    }
+}
---
+++ Students/Models/dbContext.cs
@@ -0,0 +1,22 @@
+namespace Students.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class dbContext : DbContext
+    {
+        public dbContext()
+            : base("name=dbContext")
+        {
+
+        }
+
+        public DbSet<Infor> Infors { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
